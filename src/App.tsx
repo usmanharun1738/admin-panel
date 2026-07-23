131a8f578b7de7196feb037afa4b046cb77cb80c
usman harun <usmanharuna1738@gmail.com>
2026-07-23 10:42:49 +0100

    init: project set up  for admin panel

---
+++ src/App.tsx
@@ -0,0 +1,65 @@
+import {
+  Refine,
+  GitHubBanner,
+  WelcomePage,
+  Authenticated,
+} from "@refinedev/core";
+import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
+import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
+
+import {
+  AuthPage,
+  ErrorComponent,
+  useNotificationProvider,
+  ThemedLayout,
+  ThemedSider,
+} from "@refinedev/antd";
+import "@refinedev/antd/dist/reset.css";
+
+import { App as AntdApp } from "antd";
+import { BrowserRouter, Route, Routes, Outlet } from "react-router";
+import routerProvider, {
+  NavigateToResource,
+  CatchAllNavigate,
+  UnsavedChangesNotifier,
+  DocumentTitleHandler,
+} from "@refinedev/react-router";
+import { dataProvider } from "./providers/data";
+import { ColorModeContextProvider } from "./contexts/color-mode";
+import { Header } from "./components/header";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <GitHubBanner />
+      <RefineKbarProvider>
+        <ColorModeContextProvider>
+          <AntdApp>
+            <DevtoolsProvider>
+              <Refine
+                dataProvider={dataProvider}
+                notificationProvider={useNotificationProvider}
+                routerProvider={routerProvider}
+                options={{
+                  syncWithLocation: true,
+                  warnWhenUnsavedChanges: true,
+                  projectId: "nM9gn4-BdypCb-zx7xJT",
+                }}
+              >
+                <Routes>
+                  <Route index element={<WelcomePage />} />
+                </Routes>
+                <RefineKbar />
+                <UnsavedChangesNotifier />
+                <DocumentTitleHandler />
+              </Refine>
+              <DevtoolsPanel />
+            </DevtoolsProvider>
+          </AntdApp>
+        </ColorModeContextProvider>
+      </RefineKbarProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
